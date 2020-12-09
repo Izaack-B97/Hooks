01100333e4f2f6eb0c9b35f13839286866191ebc
Isaac Bustamante <izaack.97@gmail.com>
2020-12-09 09:36:37 -0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CounterApp } from './Components/01-useState/CounterApp';
+import './index.css';
+// import { HookApp } from './HookApp';
+
+ReactDOM.render(
+  <CounterApp />,
+  document.getElementById('root')
+);
+
