56c008048d7eaab42a9734a95223f7d75a9cd099
Isaac Bustamante <izaack.97@gmail.com>
2020-12-09 10:20:10 -0700

    Added custom hook

---
+++ src/Components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { useCounter } from '../../hooks/useCounter';
+
+import './counter.css';
+
+export const CounterWithCustomHook = () => {
+
+    const { state, increment, reset, decrement } = useCounter(100);
+
+    return (
+        <Fragment>
+            <h1>Counter with hook { state }</h1>
+            <hr />
+
+            <button onClick={ () => increment(1) } className="btn btn-primary"> +1 </button>
+            <button onClick={ reset } className="btn btn-primary"> Reset </button>
+            <button onClick={ () => decrement(1) } className="btn btn-primary"> -1 </button>
+
+        </Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './Components/01-useState/CounterApp';
-import './index.css';
+import { CounterWithCustomHook } from './Components/01-useState/CounterWithCustomHook';
+// import { CounterApp } from './Components/01-useState/CounterApp';
+// import './index.css';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <CounterApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 );
 
