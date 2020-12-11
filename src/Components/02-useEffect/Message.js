00a058feee20c07a4c070193e4bcd50d1ccae200
Isaac Bustamante <izaack.97@gmail.com>
2020-12-11 09:06:55 -0700

    Precauciones de useEffect

--- src/Components/02-useEffect/Message.js
+++ src/Components/02-useEffect/Message.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+
+    const [coors, setCoors] = useState({ x:0, y:0 });
+    const { x, y } = coors;
+
+    const moveMouse = (e) => {
+        const coords = { x: e.x, y: e.y };
+        // setCoors(coords);
+        console.log(coords);
+    }; 
+
+    window.addEventListener('mousemove', moveMouse);
+
 
     useEffect(() => {
         console.log('Componente montado');
         
         return () => {
-            console.log('Componente desmontado');
+            // Area de limpieza
+            window.removeEventListener('mousemove', moveMouse);
         }
     }, []);
 
     return (
         <div>
             <h1>Eres genial !</h1>
+            <p>
+                x: { x } y: { y }
+            </p>
         </div>
     )
 }
