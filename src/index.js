0316b9314b2b78a159355787f39d1de1c3e9c8cf
Isaac Bustamante <izaack.97@gmail.com>
2021-01-06 12:49:17 -0700

    general idea of reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './Components/07-tarea-memo/Padre';
-// import { CallbackHook } from './Components/06-memos/CallbackHook';
-// import { MemoHook } from './Components/06-memos/MemoHook';
-// import { Memorize } from './Components/06-memos/Memorize';
-// import { Layout } from './Components/05-useLayoutEffect/Layout';
-// import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
-// import { FocusScreen } from './Components/04-useRef/FocusScreen';
-// import { MultiplesCustomHooks } from './Components/03-examples/MultiplesCustomHooks';
-// import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
-// import { SimpleForm } from './Components/02-useEffect/SimpleForm';
-// import { CounterWithCustomHook } from './Components/01-useState/CounterWithCustomHook';
-// import { CounterApp } from './Components/01-useState/CounterApp';
-// import { HookApp } from './HookApp';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './Components/07-tarea-memo/Padre';
+// // import { CallbackHook } from './Components/06-memos/CallbackHook';
+// // import { MemoHook } from './Components/06-memos/MemoHook';
+// // import { Memorize } from './Components/06-memos/Memorize';
+// // import { Layout } from './Components/05-useLayoutEffect/Layout';
+// // import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
+// // import { FocusScreen } from './Components/04-useRef/FocusScreen';
+// // import { MultiplesCustomHooks } from './Components/03-examples/MultiplesCustomHooks';
+// // import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
+// // import { SimpleForm } from './Components/02-useEffect/SimpleForm';
+// // import { CounterWithCustomHook } from './Components/01-useState/CounterWithCustomHook';
+// // import { CounterApp } from './Components/01-useState/CounterApp';
+// // import { HookApp } from './HookApp';
 
-import './index.css';
+// import './index.css';
 
 
-ReactDOM.render(
-  <Padre />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Padre />,
+//   document.getElementById('root')
+// );
 
+import './Components/08-useReducer/intro-reducer'
